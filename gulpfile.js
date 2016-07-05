690f065c78670cc53ac1c050e0fe20b2a2b3ab4c
funtrum <funtrum@Nicks-MacBook-Pro.local>
2016-07-05 03:02:37 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,162 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    jade = require('gulp-jade'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload,
+    rename = require("gulp-rename");
+
+    var path = {
+    build: { //Тут мы укажем куда складывать готовые после сборки файлы
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    src: { //Пути откуда брать исходники
+        jade: 'src/*.jade', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js: 'src/js/main.js',//В стилях и скриптах нам понадобятся только main файлы
+        style: 'src/style/main.sass',
+        normalize: 'bower_components/normalize-css/normalize.css',
+        jquery: 'bower_components/jquery/dist/jquery.min.*',
+        img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        jade: 'src/**/*.jade',
+        html: 'build/**/*.html',
+        js: 'src/js/**/*.js',
+        style: 'src/style/**/*.sass',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './build'
+};
+
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: false,
+    host: 'localhost',
+    port: 9000,
+    reloadDelay: 1000,
+    logPrefix: "Page"
+};
+
+gulp.task('bs-reload', function() {
+    browserSync.reload();
+});
+
+gulp.task('jade:build', function(){
+  gulp.src(path.src.jade)
+    .pipe(jade({pretty: true}))
+    .pipe(gulp.dest(path.build.html))
+});
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js) //Найдем наш main файл
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(sourcemaps.init()) //Инициализируем sourcemap
+        .pipe(uglify()) //Сожмем наш js
+        .pipe(rename({suffix: '.min'})) // Префикс min
+        .pipe(sourcemaps.write()) //Пропишем карты
+        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
+        .pipe(reload({stream: true})); //И перезагрузим сервер
+});
+
+gulp.task('style:build', function () {
+    gulp.src(path.src.style) //Выберем наш main.scss
+        .pipe(sourcemaps.init()) //То же самое что и с js
+        .pipe(sass()) //Скомпилируем
+        .pipe(prefixer()) //Добавим вендорные префиксы
+        .pipe(cssmin()) //Сожмем
+        .pipe(rename({suffix: '.min'})) // Префикс min
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css)) //И в build
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('normalize:build', function () {
+    gulp.src(path.src.normalize) //Выберем наш main.scss
+        .pipe(sourcemaps.init()) //То же самое что и с js
+        .pipe(cssmin()) //Сожмем
+        .pipe(rename({suffix: '.min'})) // Префикс min
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css)) //И в build
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('image:build', function () {
+    gulp.src(path.src.img) //Выберем наши картинки
+        .pipe(imagemin({ //Сожмем их
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img)) //И бросим в build
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('jquery:build', function() {
+    gulp.src(path.src.jquery)
+    .pipe(gulp.dest(path.build.js))
+});
+
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+    .pipe(gulp.dest(path.build.fonts))
+});
+
+gulp.task('build', [
+    'jade:build',
+    'js:build',
+    'style:build',
+    'jquery:build',
+    'normalize:build',
+    'fonts:build',
+    'image:build'
+]);
+
+gulp.task('watch', function(){
+    watch([path.watch.jade], function(event, cb) {
+        gulp.start('jade:build');
+    });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('style:build');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+    watch([path.watch.img], function(event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('bs-reload');
+    });
+});
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+gulp.task('default', ['build', 'webserver', 'watch']);
---
+++ src/js/main.js
@@ -0,0 +1,88 @@
+updateInterval = 60 // каждую минуту
+
+function parseStatus(url) {
+	$.ajax({
+		method: "POST",
+		url: url,
+		data: { name: "John", location: "Boston" }
+	})
+	.done(function(msg) {
+		var msgFormatted = msg.substring(36).slice(0, -2);
+		selectMoscow(JSON.parse(msgFormatted.replace(/(['"])?([a-zA-Z_]+)(['"])?:/g, '"$2": ')));
+	});
+}
+
+$(document).ready(function(){
+	parseStatus('http://nickaroot.me/trafficlight/parse.php');
+});
+
+function selectMoscow(json) {
+	var regions = json.regions;
+	regions.forEach(function(item) {
+		if (item.regionId == "213") {
+			updateStatus(item);
+		}
+	});
+}
+
+function updateStatus(region) {
+	var level = region.level;
+	var road = 0;
+	var style = region.style;
+	
+	if (level >= 0 && level <= 1) {
+		road = 1;
+	} else if (level >= 2 && level <= 3) {
+		road = 2;
+	} else if (level >= 4 && level <= 5) {
+		road = 3;
+	} else if (level >= 6 && level <= 7) {
+		road = 4;
+	} else if (level >= 8 && level <= 9) {
+		road = 5;
+	} else if (level >= 10) {
+		road = 6;
+	}
+
+	console.log(road);
+
+	for(var i = 1; i <= 6; i++) {
+		if (i == road) {
+			$("#road_"+i).removeClass("bounceOutDown").addClass("animated bounceInUp active");
+			setTimeout(function(){$("#road_"+i).removeClass("bounceInUp")}, 1000);
+		} else {
+			$("#road_"+i).addClass("bounceOutDown").removeClass("bounceInUp");
+			setTimeout(function(){$("#road_"+i).removeClass("active")}, 1000);
+		}
+	}
+
+	switch(style) {
+		case "green":
+			activateLight("green");
+			break;
+		case "yellow":
+			activateLight("yellow");
+			break;
+		case "red":
+			activateLight("red");
+			break;
+
+	}
+}
+
+function activateLight(color) {
+	var colors = ["red", "yellow", "green"];
+	colors.forEach(function(item){
+		if (item == color) {
+			$("#"+item+"Main").removeClass(item+"DeactiveMain").removeClass(item+"FlashingMain").addClass(item+"ActiveMain");
+			$("#"+item+"Add").removeClass(item+"DeactiveAdd").removeClass(item+"FlashingAdd").addClass(item+"ActiveAdd");
+			$("body").addClass(item+"ActiveBody");
+		} else {
+			$("#"+item+"Main").removeClass(item+"ActiveMain").removeClass(item+"FlashingMain").addClass(item+"DeactiveMain");
+			$("#"+item+"Add").removeClass(item+"ActiveAdd").removeClass(item+"FlashingAdd").addClass(item+"DeactiveAdd");
+			$("body").removeClass(item+"ActiveBody");
+		}
+	});
+	console.log(color+" is activated");
+	setTimeout('parseStatus("http://nickaroot.me/trafficlight/parse.php")', updateInterval*1000);
+}
